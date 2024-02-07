4efbc4eed11c6c4f1b3b04a7a0778b79c7b05db0
Christian Manzaraz <magnusmanz@gmail.com>
2024-02-07 11:41:19 -0300

    Initial Commit

---
+++ MyFirstMap/MyFirstMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstMapApp.swift
+//  MyFirstMap
+//
+//  Created by Christian Manzaraz on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
